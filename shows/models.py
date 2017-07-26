db471996dbed2f81952cc8f1e8142e0434284d49
Lars Kappert <lars@webpro.nl>
2017-07-26 15:28:37 +0200

    Add shows srtuff

--- shows/models.py
+++ shows/models.py
@@ -7,6 +7,8 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailsearch import index
+
 
 
 class ShowsIndexPage(Page):
@@ -14,4 +16,26 @@ class ShowsIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+    ]
+
+
+# Keep the definition of BlogIndexPage, and add:
+
+
+class ShowPage(Page):
+    showname = models.CharField(max_length=250)
+    showdescription = RichTextField(blank=True)
+    automationID = RichTextField(blank=True)
+    profileID = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('showname'),
+        index.SearchField('showdescription'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('showname'),
+        FieldPanel('showdescription'),
+        FieldPanel('automationID'),
+        FieldPanel('profileID', classname="full"),
     ]
